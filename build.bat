29c741440be12f0b6592e5b747365741c75f8a7b
Piotr Juszczyk <piotr.a.juszczyk@gmail.com>
2021-12-28 19:59:50 +0100

    Clean up placement of build output

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
-del laplace
-del laplace.exp
-del laplace.lib
-del kernels.obj
-nvcc -c kernels.cu
-nvcc -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64\cl.exe" -rdc=true kernels.obj main.cpp -o laplace.exe -luser32 -lGdi32
+cd bin
+if exist laplace.exe del laplace.exe
+if exist laplace.exp del laplace.exp
+if exist laplace.lib del laplace.lib
+if exist kernels.obj del kernels.obj
+nvcc -c ..\kernels.cu
+nvcc -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64\cl.exe" -rdc=true kernels.obj ..\main.cpp -o laplace.exe -luser32 -lGdi32
+cd ..
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rm laplace -f
-rm kernels.o -f
-nvcc -c kernels.cu
-nvcc -ccbin g++ -Xcompiler "-std=c++11" kernels.o main.cpp -lcuda -lcudart -o laplace -lm -lpthread -lX11
+rm bin/laplace -f
+rm bin/kernels.o -f
+nvcc -c kernels.cu -o bin/kernels.o
+nvcc -ccbin g++ -Xcompiler "-std=c++11" bin/kernels.o main.cpp -lcuda -lcudart -o bin/laplace -lm -lpthread -lX11
