35e0e060b6278ed7ade7b2bd0e66017a6019827b
Piotr Juszczyk <piotr.a.juszczyk@gmail.com>
2021-12-28 19:17:56 +0100

    Fix nvcc options in build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
+del laplace
+del laplace.exp
+del laplace.lib
+del kernels.obj
 nvcc -c kernels.cu
-nvcc -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\bin\Hostx64\x64\lc.exe" -rdc=true kernels.obj main.cpp -o out.exe -lm -lpthread -lX11
+nvcc -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64\cl.exe" -rdc=true kernels.obj main.cpp -o laplace.exe -luser32 -lGdi32
